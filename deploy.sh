a062eaa86b804ffdbef82e6e00bff201f1f2e9fd
Clement Denis <clement@01talent.com>
2024-09-17 12:19:09 +0100

    add missing shebang

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 wget https://github.com/redlib-org/redlib/releases/download/v0.31.0/redlib
 chmod +x redlib
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 export REDLIB_PORT=$PORT
 export REDLIB_DEFAULT_WIDE=on
 export REDLIB_DEFAULT_USE_HLS=on
@@ -7,4 +9,5 @@ export REDLIB_DEFAULT_COMMENT_SORT=top
 export REDLIB_DEFAULT_SHOW_NSFW=on
 export REDLIB_DEFAULT_POST_SORT=top
 export REDLIB_DEFAULT_THEME=dracula
+
 ./redlib
